0f6b44bc6aabd0a2cc5453bfc9be2deb0a80be44
Robert Willian Polli <51081790+robertpolli@users.noreply.github.com>
2022-10-20 08:52:41 -0300

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python-sirius
 from Adafruit_BBIO.SPI import SPI
+
 #-------------------------------------------------------
 # initialize the bus and device /dev/spidev1.0
 spi = SPI(0,0)
+#defining mode (CPOL = 0; CPHA = 1)
+spi.mode = 1
+#defining speed (in bps)
+spi.msh = 10000000
 #-------------------------------------------------------
+
 '''
 !!! IMPORTANT INFORMATION !!!
 1. For sending data to the register, the SPI mode should be set to 0
