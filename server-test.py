31441f38f2fbb8ab12febeac146a2d51359dd119
Robert Willian Polli <51081790+robertpolli@users.noreply.github.com>
2022-10-26 15:19:18 -0300

    Added "start" readback

--- server-test.py
+++ server-test.py
@@ -14,6 +14,10 @@ def sendVariable(variableID, size, function, valueToWrite = 0):
     
     if (function == "R"):
         send_message = [0x00, 0x10] + [c for c in struct.pack("!h",size+1)] + [variableID]
+        
+    elif (func == "W" and (com in [32, 33, 34, 35])):
+        send_message = [0x00, 0x20] + [c for c in struct.pack("!h", size + 1)] + [variableID]
+
     else:
         send_message = [0x00, 0x20] + [c for c in struct.pack("!h",size+1)] + [variableID] + [valueToWrite]
     return("".join(map(chr,includeChecksum(send_message))))
@@ -33,12 +37,15 @@ while(True):
     com = int(input(f"Digite o comando de {command[func]}: \n"), 16)
     
     if(func == "R"):
-        tcp.send(sendVariable(com, size = 1, function = func).encode())
-        print([ord(i) for i in tcp.recv(128).decode("latin-1")]) 
+        tcp.send(sendVariable(com, size = 1, function = func).encode()) 
        
-    elif(func == "W"):
+    elif(func == "W" and (com not in [32, 33, 34, 35])):
         value = int(input("Digite o valor: \n"), 16)
         tcp.send(sendVariable(com, size = 2, function = func, valueToWrite = value).encode())
     
+    else: 
+        tcp.send(sendVariable(com, size=1, function=func).encode())
+        
+    print([ord(i) for i in tcp.recv(128).decode("latin-1")])
     
         
