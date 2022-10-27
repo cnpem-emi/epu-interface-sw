968c6270d57888e1ab24d00bc7e2af211b9123aa
Robert Willian Polli <robert.polli@cnpem.br>
2022-10-27 13:54:37 -0300

    New service and files organization

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+echo "Exporting SPI pins"
+
+./configpin.sh
+
+echo "Setting RS485 interface"
+
+echo "Starting socket"
+
+pushd scripts
+    ./epusocket.py
+
+popd
